7e49c7911f9e4e8b2825235470375d5fcaa0258b
Jean-Luc Charles <jean-luc.charles@mailo.com>
2024-07-22 10:14:07 +0200

    Update plot scripts

--- ROTOR_bench/Processing/plot_FREE.py
+++ ROTOR_bench/Processing/plot_FREE.py
@@ -1,4 +1,4 @@
-from tools import get_files_by_date, read_file_ROTOR, read_file_FREE, plot_magField
+from tools import get_files_by_date, read_file_FREE, plot_magField
 import matplotlib.pyplot as plt
 import numpy as np
 import sys, os
@@ -8,18 +8,22 @@ from stat import ST_CTIME
 if __name__ == "__main__":
     
     import argparse
-    
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', action="store", dest='data_dir', 
                          help="Optional, the relative path of the data directory")
+    parser.add_argument('-a', '--all', action="store_true", dest='all_file', 
+                         help="Optional, to draw the plots for all of the .txt in the directory")
+    
     args = parser.parse_args()
-    data_dir = "./" if not args.data_dir else args.data_dir
-        
+    data_dir = "./TXT" if not args.data_dir else args.data_dir
+    all_file = args.all_file
+    
     list_file = get_files_by_date(data_dir, 'FREE')
 
     if not list_file:
        print(f"No .txt file found in directory <{data_dir}>, tchao")
-    else:    
+       
+    elif not all_file: 
         while True:
             for i, file in enumerate(list_file):
                 print(f'{i:2d} -> {os.path.join(data_dir, file)}')
@@ -38,3 +42,11 @@ if __name__ == "__main__":
             # plot the data
             plot_magField(T, magnField, fileName, figsize=(10,8))
 
+    else:
+        for f in list_file:
+            fileName = os.path.join(data_dir, f)
+            DATA = read_file_FREE(fileName)
+            # transpose DATA to extract the different variables:
+            T, magnField = DATA.T[0], DATA.T[1:]            
+            # plot the data
+            plot_magField(T, magnField, fileName, figsize=(10,8), show=False)
--- ROTOR_bench/Processing/tools.py
+++ ROTOR_bench/Processing/tools.py
@@ -50,36 +50,44 @@ def read_file_FREE(fileName):
     DATA = np.array(DATA)
     return DATA
     
-def plot_magField(T, field, filename, figsize=(8,6)):
+def plot_magField(T, field, filename, figsize=(8,6), stat=None, show=True):
 
     dirname = os.path.dirname(filename)
     filename = os.path.basename(filename)
 
     X, Y, Z = field
     sigmaX, sigmaY, sigmaZ = X.std(), Y.std(), Z.std()
+    if stat == None:
+        if sigmaX > 0.5 or sigmaY > 0.5 or sigmaZ > 0.5:
+            stat = False
+        else:
+            stat = True
     
-    fig, ax = plt.subplots(1, 1, figsize=figsize, sharex=True)
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
     fig.suptitle(f"Rotor magnetic field", size=16)
-    ax.set_title(f"from file <{filename}>", color="grey", size=12)
+    fig.text(0.5, .92, f"from file <{filename}>", size=10, color="gray",
+                horizontalalignment='center')
     ax.plot(T, X, '-or', markersize=0.5, label='X')
     ax.plot(T, Y, '-og', markersize=0.5, label='Y')
     ax.plot(T, Z, '-ob', markersize=0.5, label='Z')
     ax.legend(bbox_to_anchor=(1.15, 1), loc="upper right")
     ymean = np.array(ax.get_ylim()).mean()
     yp2p = np.ptp(np.array(ax.get_ylim()))
-    ax.text(1.07*T.max(), ymean, r"$\sigma_X$: " + f"{sigmaX:5.2e} mT", color='r')
-    ax.text(1.07*T.max(), ymean - 0.06*yp2p, r"$\sigma_Y$: " + f"{sigmaY:5.2e} mT", color='g')
-    ax.text(1.07*T.max(), ymean - 0.12*yp2p, r"$\sigma_Z$: " + f"{sigmaZ:5.2e} mT", color='b')
+    if stat:
+        ax.text(1.07*T.max(), ymean, r"$\sigma_X$: " + f"{sigmaX:5.2e} mT", color='r')
+        ax.text(1.07*T.max(), ymean - 0.06*yp2p, r"$\sigma_Y$: " + f"{sigmaY:5.2e} mT", color='g')
+        ax.text(1.07*T.max(), ymean - 0.12*yp2p, r"$\sigma_Z$: " + f"{sigmaZ:5.2e} mT", color='b')
     ax.grid()
     ax.set_ylabel("[mT]")
     ax.set_xlabel("Time[s]")
     plt.subplots_adjust(right=0.84)
     figPath = os.path.join(dirname, filename.replace('.txt', '.png'))
-    print(figPath)
+    if show == False: print(figPath)
     plt.savefig(figPath)
-    plt.show()
+    if show: plt.show()
+    plt.close()
 
-def plot_magField_at_positions(A, field, list_pos, filename, figsize=(8,6)):
+def plot_magField_at_positions(A, field, list_pos, filename, figsize=(8,6), show=True):
 
     nb_Zpos = len(list_pos)
     nb_comp, nb_angle_pos = field.shape
@@ -88,20 +96,21 @@ def plot_magField_at_positions(A, field, list_pos, filename, figsize=(8,6)):
     dirname = os.path.dirname(filename)
     filename = os.path.basename(filename)
 
-    fig, axes = plt.subplots(nb_Zpos, 1, figsize=figsize, sharex=True)
-    fig.suptitle(f"Rotor magnetic field\nfrom file <{filename}>", size=16)
-
+    fig, axes = plt.subplots(nb_Zpos, 1, figsize=figsize, sharex=False)
+    fig.suptitle(f"Rotor magnetic field", size=16)
+    fig.text(0.5, .92, f"from file <{filename}>", size=10, color="gray",
+                horizontalalignment='center')
     for n, p in enumerate(list_pos):
       if nb_Zpos >=2:
           ax = axes[n]
       else:
           ax = axes
-      
+
       X, Y, Z = field[3*n:3*n+3]
       ax.plot(A, X, '-or', markersize=0.5, label='X')
       ax.plot(A, Y, '-og', markersize=0.5, label='Y')
       ax.plot(A, Z, '-ob', markersize=0.5, label='Z')
-      ax.set_title(f"Magnetic field at position #{n+1}: {p} mm")
+      ax.set_title(f"Magnetic field at position #{n+1}: {int(p):3d} mm")
       ax.legend(bbox_to_anchor=(1.15, 1), loc="upper right")
       ax.grid()
       ax.set_ylabel("[mT]")
@@ -109,42 +118,10 @@ def plot_magField_at_positions(A, field, list_pos, filename, figsize=(8,6)):
       if n == nb_Zpos-1:
          ax.set_xlabel("rotor angle [°]")
 
-    plt.subplots_adjust(right=0.86)
+    plt.subplots_adjust(right=0.86, hspace=0.4)
     figPath = os.path.join(dirname, filename.replace('.txt', '.png'))
-    print(figPath)
+    if show == False: print(figPath)
     plt.savefig(figPath)
-    plt.show()
+    if show: plt.show()
+    plt.close()
     
-if __name__ == "__main__":
-    
-    import argparse
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dir', action="store", dest='data_dir', 
-                         help="Optional, the relative path of the data directory")
-    args = parser.parse_args()
-    data_dir = "./" if not args.data_dir else args.data_dir
-        
-    list_file = get_files_by_date(data_dir, 'ROTOR')
-
-    if not list_file:
-       print(f"No .txt file found in directory <{data_dir}>, tchao")
-    else:    
-        while True:
-            for i, file in enumerate(list_file):
-                print(f'{i:2d} -> {os.path.join(data_dir, file)}')
-            rep = input("Fichier choisi [Q->Quit, RET->Choose last file]: ")
-            if rep.lower() == 'q':
-                break
-            elif rep == "":
-                i = len(list_file)-1
-            else:
-                i = int(rep)
-
-            fileName = os.path.join(data_dir, list_file[i])
-            DATA, list_pos = read_file_ROTOR(fileName)
-            # transpose DATA to extract the different variables:
-            A, magnField = DATA.T[0], DATA.T[1:]        
-            # plot the data
-            plot_magField_at_positions(A, magnField, list_pos, fileName, figsize=(10,8))
-
